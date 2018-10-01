4e4404ad175a388af1922da2d2e10ff294613595
RePorcforuliitD <37216730+RePorcforuliitD@users.noreply.github.com>
2018-10-02 02:30:00 +0900

    トップページを新規作成

---
+++ docs/main.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: "#app",
+    data: {
+
+        list: [
+            { id: 0, name: "Password Generator", url: "password_generator", img: "lock.png" },
+            { id: 1, name: "BMI Calculater", url: "bmi_calculater", img: "health.png" },
+        ],
+    },
+})
